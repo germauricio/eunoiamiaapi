d8d2b7a8b8ae158775077daa4532ea6d02dec242
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-08-08 03:45:33 -0300

    add other headers

--- api.js
+++ api.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 const apiProductsRouter = require('./api/productsRouter');
 
+router.use(function(req, res, next) {
+    req.header('Access-Control-Allow-Origin', '*');
+    next();
+ });
+
 router.get('/products', apiProductsRouter);
 router.get('/product', apiProductsRouter);
 router.put('/product', apiProductsRouter);
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
-
 const apiRouter = require('./api');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 require('./services/db');
 
