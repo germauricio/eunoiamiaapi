3180c7a80f96d22a171fd1ca3e2104abf933c976
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-08-08 01:20:49 -0300

    api build

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ require('./services/db');
 
 app.use('/api', apiRouter);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.listen(process.env.PORT || 5000)
