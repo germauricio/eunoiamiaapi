bc0c24be89f8dbe4a9bc0f8d2a29939f515e87da
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-08-08 03:18:52 -0300

    add other headers

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./services/db');
 app.use('/api', apiRouter);
 
 app.use((req, res, next) => {
-    req.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    req.header('Access-Control-Allow-Headers', 'Origin');
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
